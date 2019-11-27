e167f1f645e488857149bd53ccb1c36fc8e31afa
Talon120 <taloncjones@gmail.com>
2019-11-26 19:51:00 -0800

    Add toggle line-through checkbox functionality

--- src/App.js
+++ src/App.js
@@ -23,10 +23,20 @@ class App extends Component {
     ]
   }
 
+  // Toggle Complete
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+    }) });
+  }
+
   render () {
     return (
       <div className="App">
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
       </div>
     );
   }
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,14 +3,10 @@ import TodoItem from './TodoItem';
 import PropTypes from 'prop-types';
 
 class Todos extends Component {
-  markComplete = () => {
-    console.log("Hello")
-  }
-
   render () {
     return this.props.todos.map((todo) => (
       <TodoItem key={todo.id} todo={todo} markComplete=
-      {this.markComplete}/>
+      {this.props.markComplete}/>
     ));
   }
 }
