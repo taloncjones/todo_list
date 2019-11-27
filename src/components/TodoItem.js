1eec14cbcbae53dd2a4a315fe596d324ea99977e
Talon120 <taloncjones@gmail.com>
2019-11-26 19:39:30 -0800

    Add CSS to todo items

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,14 +3,12 @@ import PropTypes from 'prop-types';
 
 export class TodoItem extends Component {
   getStyle = () => {
-    if(this.props.todo.completed) {
-      return {
-        textDecoration: 'line-through'
-      }
-    } else {
-      return {
-        textDecoration: 'none'
-      }
+    return {
+      background: '#f4f4f4',
+      padding: '10px',
+      borderBottom: '1px #ccc dotted',
+      textDecoration: this.props.todo.completed ? 
+      'line-through' : 'none'
     }
   }
 
