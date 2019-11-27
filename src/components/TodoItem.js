f10f1e812e6068b5e15ee4b58501b3dc1940a95f
Talon120 <taloncjones@gmail.com>
2019-11-26 19:53:56 -0800

    Add delete button object

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -20,6 +20,7 @@ export class TodoItem extends Component {
           <input type="checkbox" onChange=
           {this.props.markComplete.bind(this, id)} /> {' '}
           { title }
+          <button style={btnStyle}>x</button>
         </p>
       </div>
     )
@@ -29,6 +30,16 @@ export class TodoItem extends Component {
 // PropTypes
 TodoItem.propTypes = {
   todo: PropTypes.object.isRequired
+}
+
+const btnStyle = {
+  background: '#ff0000',
+  color: '#fff',
+  border: 'none',
+  padding: '5px 8px',
+  borderRadius: '50%',
+  cursor: 'pointer',
+  float: 'right'
 }
 
 export default TodoItem
