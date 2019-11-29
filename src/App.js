65cb992fa9e380397515dc23681c33bf36239ea8
Talon120 <taloncjones@gmail.com>
2019-11-28 20:49:27 -0800

    Add API call to delete/remove todo item

--- src/App.js
+++ src/App.js
@@ -31,8 +31,9 @@ class App extends Component {
 
   // Delete Todo
   delTodo = (id) => {
-    this.setState({ todos: [...this.state.todos.filter(todo =>
-      todo.id !== id)] });
+    axios.delete('https://jsonplaceholder.typicode.com/todos/${id}')
+      .then(res => this.setState({ todos: [...this.state.todos.filter(todo =>
+        todo.id !== id)] }));
   }
 
   // Add Todo
